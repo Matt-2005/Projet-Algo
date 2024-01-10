31f13c6ba50e1f7e1a8d28da51f31e952dde0539
Matt-2005 <76447374+Matt-2005@users.noreply.github.com>
2024-01-10 11:25:47 +0100

    Alternance player1-2 fonctionnelle

--- classes.py
+++ classes.py
@@ -11,34 +11,28 @@ class Pion:
     def getY(self):
         return self.__y
 
-
     def setX(self, newX):
         self.__x = newX
     def setY(self, newY):
         self.__y = newY
-    
+
     def getPlayer(self):
         return self.__player
-    
+
     def setPlayer(self, newPlayer):
         self.__player = newPlayer
 
-
     def updatePlayer(self):
-        if self.getPlayer() == 1:
+        if self.__player == 1:
             self.setPlayer(2)
         else:
             self.setPlayer(1)
-        
 
 class Jeu:
     def __init__(self, boardSize, pion):
         self.__boardSize = boardSize
         self.pion = pion
         self.__Board = self.plateau(boardSize)
-        
-    def getBoardsize(self):
-        return self.__boardSize
 
     def plateau(self, boardSize):
         board = []
@@ -48,69 +42,53 @@ class Jeu:
                 line.append(0)
             board.append(line)
         return board
-    
+
     def moveCondition(self):
         x = self.pion.getX()
         y = self.pion.getY()
         possibleMove = []
         possibleCoords = [(2, 1), (2, -1), (-2, 1), (-2, -1), (1, 2), (1, -2), (-1, 2), (-1, -2)]
-        for possibleCoordsX, possibleCoordsY in possibleCoords:
-            newX, newY = x + possibleCoordsX, y + possibleCoordsY
-            if 0 <= newX < self.getBoardsize() and 0 <= newY < self.getBoardsize() and self.__Board[newX][newY] == 0:
+        for dx, dy in possibleCoords:
+            newX, newY = x + dx, y + dy
+            if 0 <= newX < self.__boardSize and 0 <= newY < self.__boardSize and self.__Board[newX][newY] == 0:
                 possibleMove.append((newX, newY))
         print(possibleMove)
-        print(self.__Board[newX][newY])
-        print(x, y)
-        print(pion.getPlayer())
+        print(self.pion.getPlayer())
         return possibleMove
-    
-    def updateBoard(self, x, y):
-        possibleMove = self.moveCondition()
-        for coords in possibleMove:
-            if (x, y) == coords:
-                self.__Board[x][y] = self.pion.getPlayer()
-                self.pion.setX(x)
-                self.pion.setY(y)
-                pion.updatePlayer()
-                return True
-            else:
-                return False
-                
-        
 
+    def updateBoard(self, x, y):
+        if (x, y) in self.moveCondition():
+            self.__Board[x][y] = self.pion.getPlayer()
+            self.pion.setX(x)
+            self.pion.setY(y)
+            return True
+        return False
 
 class GameInterface:
     def __init__(self, boardSize):
         self.boardSize = boardSize
         self.pion = Pion(0, 0, 1)
-        self.jeu = Jeu(boardSize, pion)
-        self.player = 1
+        self.jeu = Jeu(boardSize, self.pion)
 
         self.window = Tk()
         self.window.title("Jeu de Puissance 5")
 
-
-        self.buttons = []
+        self.buttons = [[Button(self.window, width=4, height=2, command=lambda i=i, j=j: self.place_pion(i, j)) for j in range(boardSize)] for i in range(boardSize)]
         for i in range(boardSize):
-            row = []
             for j in range(boardSize):
-                button = Button(self.window, width=4, height=2, command=lambda i=i, j=j: self.place_pion(i, j))
-                button.grid(row=i, column=j)
-                row.append(button)
-            self.buttons.append(row)
+                self.buttons[i][j].grid(row=i, column=j)
 
         self.window.mainloop()
-        
 
     def place_pion(self, x, y):
-        self.pion.setX(x)
-        self.pion.setY(y)
-        if self.jeu.updateBoard(x, y) == True:
-            self.buttons[x][y].config(bg='green')
+        if self.jeu.updateBoard(x, y):
+            player = self.pion.getPlayer()
+            if player == 1:
+                self.buttons[x][y].config(bg='green')
+            else:
+                self.buttons[x][y].config(bg='blue')
+            self.pion.updatePlayer()
         else:
             self.buttons[x][y].config(bg='red')
-            
-            
-pion = Pion(0,0,1)    
-game = Jeu(10, pion)
+
 game = GameInterface(10)
