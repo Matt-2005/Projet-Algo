baeb6f2e56a4180852a572df8b27c841acc12994
Matt-2005 <76447374+Matt-2005@users.noreply.github.com>
2024-01-11 09:08:02 +0100

    avancée alternance player

--- classes.py
+++ classes.py
@@ -1,7 +1,7 @@
 from tkinter import *
 
 class Pion:
-    def __init__(self, abscisse, ordonnee, player):
+    def __init__(self, abscisse = -1, ordonnee = -1, player = 1):
         self.__x = abscisse
         self.__y = ordonnee
         self.__player = player
@@ -43,31 +43,40 @@ class Jeu:
             board.append(line)
         return board
 
+
     def moveCondition(self):
         x = self.pion.getX()
         y = self.pion.getY()
         possibleMove = []
         possibleCoords = [(2, 1), (2, -1), (-2, 1), (-2, -1), (1, 2), (1, -2), (-1, 2), (-1, -2)]
-        for dx, dy in possibleCoords:
-            newX, newY = x + dx, y + dy
+        for possibleCoordsX, possibleCoordsY in possibleCoords:
+            newX, newY = x + possibleCoordsX, y + possibleCoordsY
             if 0 <= newX < self.__boardSize and 0 <= newY < self.__boardSize and self.__Board[newX][newY] == 0:
                 possibleMove.append((newX, newY))
         print(possibleMove)
         print(self.pion.getPlayer())
         return possibleMove
 
+    
     def updateBoard(self, x, y):
         if (x, y) in self.moveCondition():
-            self.__Board[x][y] = self.pion.getPlayer()
+            currentPlayer = self.pion.getPlayer()
+            self.__Board[x][y] = currentPlayer
             self.pion.setX(x)
             self.pion.setY(y)
+
+            # Mise à jour de l'état de jeu pour le prochain joueur
+            self.pion.updatePlayer()  # Change le joueur actif
+            nextPlayerMoves = self.moveCondition()  # Calcule les coups possibles pour le joueur suivant
+            print(f"Possible moves for player {self.pion.getPlayer()}: {nextPlayerMoves}")
+            
             return True
         return False
 
 class GameInterface:
     def __init__(self, boardSize):
         self.boardSize = boardSize
-        self.pion = Pion(0, 0, 1)
+        self.pion = Pion()
         self.jeu = Jeu(boardSize, self.pion)
 
         self.window = Tk()
@@ -77,11 +86,17 @@ class GameInterface:
         for i in range(boardSize):
             for j in range(boardSize):
                 self.buttons[i][j].grid(row=i, column=j)
+        self.debutJeu()
 
         self.window.mainloop()
 
+    def debutJeu(self):
+        print("Début du jeu. Les joueurs peuvent placer leur premier pion où ils veulent.")
+
     def place_pion(self, x, y):
-        if self.jeu.updateBoard(x, y):
+        if self.pion.getX() == -1 and self.pion.getY() == -1:
+            self.pion.setX(x)
+            self.pion.setY(y)
             player = self.pion.getPlayer()
             if player == 1:
                 self.buttons[x][y].config(bg='green')
@@ -89,6 +104,9 @@ class GameInterface:
                 self.buttons[x][y].config(bg='blue')
             self.pion.updatePlayer()
         else:
-            self.buttons[x][y].config(bg='red')
-
+            if self.jeu.updateBoard(x, y):
+                self.buttons[x][y].config(bg='green' if self.pion.getPlayer() == 1 else 'blue')
+                self.pion.updatePlayer()
+            else:
+                self.buttons[x][y].config(bg='red')
 game = GameInterface(10)
